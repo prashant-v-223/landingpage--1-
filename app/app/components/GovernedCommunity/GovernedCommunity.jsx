0cab23d1768447e5714e79a80620485d207ea8c9
prashant-v-223 <109692632+prashant-v-223@users.noreply.github.com>
2024-08-12 19:44:36 +0530

    s
    s

---
+++ app/app/components/GovernedCommunity/GovernedCommunity.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+const GovernedCommunity = () => {
+  return (
+<div>
+    <div className='GovernedCommunity text-center py-28 container'>
+         <h4 className='text-3xl font-bold text-white'>
+          <span className='text-white'>Copywing </span> is <br/> Governed by the <br/> <span className='text-[#9B59B6]'>community</span> 
+         
+         </h4>
+         <p className='mt-5 md:w-1/3 mx-auto mb-5 text-white'>To encourage long-term holding, dividends are distributed periodically, creating a strong incentive for users to retain their CWN tokens. This aligns the interests of our community with the growth of the platform.</p>
+        
+         <a href='https://google.com' className='bg-[#9B59B6] mt-5 rounded-3xl py-2 px-4'>Click here to start buying & being a part of CWN</a>
+    </div>
+
+    
+    <div data-aos="fade-up" data-w-id="b3fd95f7-10b2-f103-e2b0-e28f97e2adb1"
+            className="df-item mt-10  df-item_last  mb-5 md:flex  justify-between"  style={{ border: 'none ' }}
+          >
+            <div className="p-8 md:ml-5 md:max-w-[55%]">
+              
+              <h3 className="text-2xl font-semibold mt-5 text-white">Why you should invest in <span className="text-[#9B59B6]">Copywing?</span></h3>
+               <ul className="mt-5">
+                  <li className="text-sm mb-3 flex">
+                       <img className="w-6 mr-4" src="/tickgreen.svg"/>
+                       <p className="text-white">Copywing is completely decentralized & works on smart contract</p> 
+                  </li>
+                  <li  className="text-sm mb-3 flex">
+                       <img className="w-6 mr-4" src="/tickgreen.svg"/>
+                    <p className="text-white">  Your investment in CWN directly benefits from the success of CopyWing.</p>   
+                  </li>
+                  <li  className="text-sm mb-3 flex">
+                       <img className="w-6 mr-4" src="/tickgreen.svg"/>
+                    <p className="text-white"> Participate in a Growing Community: Engage with fellow traders and investors who share your goals and aspirations.</p> 
+                  </li>
+                  <li  className="text-sm mb-3 flex">
+                       <img className="w-6 mr-4" src="/tickgreen.svg"/>
+                     <p className="text-white">As we expand our offerings and improve our platform, CWN holders will have a voice in the future direction of CopyWing.</p> 
+                  </li>
+               </ul>
+            </div>
+            <div className="md:max-w-[300px] pb-4 mt-4 md:mt-10 md:mx-0">
+              <img src="/images/hero-ilustration.png" />
+            </div>
+          </div>
+          </div>
+  )
+}
+
+export default GovernedCommunity
