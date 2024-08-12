35cec7083e48db0aec666f98c2089be6e4d5efdd
prashant-v-223 <109692632+prashant-v-223@users.noreply.github.com>
2024-08-12 19:47:05 +0530

    Update GovernedCommunity.jsx

--- app/components/GovernedCommunity/GovernedCommunity.jsx
+++ app/components/GovernedCommunity/GovernedCommunity.jsx
@@ -2,48 +2,285 @@ import React from 'react'
 
 const GovernedCommunity = () => {
   return (
-<div>
-    <div className='GovernedCommunity text-center py-28 container'>
-         <h4 className='text-3xl font-bold text-white'>
-          <span className='text-white'>Copywing </span> is <br/> Governed by the <br/> <span className='text-[#9B59B6]'>community</span> 
-         
-         </h4>
-         <p className='mt-5 md:w-1/3 mx-auto mb-5 text-white'>To encourage long-term holding, dividends are distributed periodically, creating a strong incentive for users to retain their CWN tokens. This aligns the interests of our community with the growth of the platform.</p>
-        
-         <a href='https://google.com' className='bg-[#9B59B6] mt-5 rounded-3xl py-2 px-4'>Click here to start buying & being a part of CWN</a>
-    </div>
+    <div>
+      <div data-aos="fade-up" data-w-id="b3fd95f7-10b2-f103-e2b0-e28f97e2adb1"
+        className="df-item mt-10  df-item_last  mb-5 md:flex  justify-between d-md-none"
+      >
+        <div className="md:p-8  pb-0 md:ml-5 md:max-w-[40%] " style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center"
+        }}>
+          <h3 className="h2" style={{
+            fontSize: 48,
+            lineHeight: "41.28px"
+          }}> About <span style={
+            {
+              color: '#775EFB',
+              fontSize: 48
+            }
+          }>Copywing</span>
+          </h3>
 
-    
-    <div data-aos="fade-up" data-w-id="b3fd95f7-10b2-f103-e2b0-e28f97e2adb1"
-            className="df-item mt-10  df-item_last  mb-5 md:flex  justify-between"  style={{ border: 'none ' }}
-          >
-            <div className="p-8 md:ml-5 md:max-w-[55%]">
-              
-              <h3 className="text-2xl font-semibold mt-5 text-white">Why you should invest in <span className="text-[#9B59B6]">Copywing?</span></h3>
-               <ul className="mt-5">
-                  <li className="text-sm mb-3 flex">
-                       <img className="w-6 mr-4" src="/tickgreen.svg"/>
-                       <p className="text-white">Copywing is completely decentralized & works on smart contract</p> 
-                  </li>
-                  <li  className="text-sm mb-3 flex">
-                       <img className="w-6 mr-4" src="/tickgreen.svg"/>
-                    <p className="text-white">  Your investment in CWN directly benefits from the success of CopyWing.</p>   
-                  </li>
-                  <li  className="text-sm mb-3 flex">
-                       <img className="w-6 mr-4" src="/tickgreen.svg"/>
-                    <p className="text-white"> Participate in a Growing Community: Engage with fellow traders and investors who share your goals and aspirations.</p> 
-                  </li>
-                  <li  className="text-sm mb-3 flex">
-                       <img className="w-6 mr-4" src="/tickgreen.svg"/>
-                     <p className="text-white">As we expand our offerings and improve our platform, CWN holders will have a voice in the future direction of CopyWing.</p> 
-                  </li>
-               </ul>
+          <h3 className="h2" style={{
+            fontSize: 40,
+            lineHeight: "50px"
+          }}> Empowering traders <br />with cutting edge tools
+          </h3>
+          <p className="p" style={{
+            color: "#C3C3C3"
+          }}>
+            <span style={
+              {
+                color: '#775EFB',
+              }
+            }>Copywing</span>, we are passionate about empowering traders with the tools they need to succeed in today's dynamic financial markets. Founded by a team of experienced traders and technology enthusiasts, we understand the challenges that traders face and are committed to providing innovative solutions to address them.
+          </p>
+        </div>
+        <div className=" ">
+          <img src="/newsectionimages/img.png" />
+        </div>
+      </div>
+      <div data-aos="fade-up" data-w-id="b3fd95f7-10b2-f103-e2b0-e28f97e2adb1"
+        className="df-item   df-item_last  d-none  justify-between d-md-flex"
+      >
+        <div className="">
+          <img src="/newsectionimages/img.png" />
+        </div>
+        <div className="p-8 pb-0  md:ml-5 md:max-w-[55%] " style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center"
+        }}>
+          <h3 className="h2" style={{
+            fontSize: 48,
+            lineHeight: "41.28px"
+          }}> About <span style={
+            {
+              color: '#775EFB',
+              fontSize: 48
+            }
+          }>Copywing</span>
+          </h3>
+
+          <h3 className="h2" style={{
+            fontSize: 40,
+            lineHeight: "50px"
+          }}> Empowering traders <br />with cutting edge tools
+          </h3>
+          <p className="p" style={{
+            color: "#C3C3C3"
+          }}>
+            <span style={
+              {
+                color: '#775EFB',
+              }
+            }>Copywing</span>, we are passionate about empowering traders with the tools they need to succeed in today's dynamic financial markets. Founded by a team of experienced traders and technology enthusiasts, we understand the challenges that traders face and are committed to providing innovative solutions to address them.
+          </p>
+        </div>
+      </div>
+      <div data-aos="fade-up" data-w-id="b3fd95f7-10b2-f103-e2b0-e28f97e2adb1"
+        className="df-item   df-item_last  d-block  justify-between d-md-flex"
+      >
+        <div className="md:max-w-[40%] " style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center"
+        }}>
+          <h3 className="h2" style={{
+            fontSize: 48,
+            lineHeight: "41.28px"
+          }}> Our  <span style={
+            {
+              color: '#775EFB',
+              fontSize: 48
+            }
+          }>mission</span>
+          </h3>
+          <h3 className="h2" style={{
+            fontSize: 40,
+            lineHeight: "50px"
+          }}> Empowering traders <br />with cutting edge tools
+          </h3>
+          <p className="p" style={{
+            color: "#C3C3C3"
+          }}>
+            Our mission is simple yet ambitious: to democratize trading and level the playing field for all investors, regardless of their experience or background. We believe that everyone should have access to powerful trading tools and resources that enable them to make informed decisions and achieve their financial goals. </p>
+        </div>
+        <div className=" " >
+          <img src="/newsectionimages/Lovepik_com-401720231-rocket 1.png" className='d-block m-auto' style={{
+            width: "400px"
+          }} />
+        </div>
+      </div>
+      <div data-aos="fade-up" data-w-id="b3fd95f7-10b2-f103-e2b0-e28f97e2adb1"
+        className="df-item   df-item_last  d-block  justify-between d-md-flex my-5 py-5"
+      >
+        <div className="md:p-8 pb-0  md:ml-5 md:max-w-[40%] " style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center"
+        }}>
+          <h3 className="h2" style={{
+            fontSize: 48,
+            lineHeight: "50px"
+          }}> Why you should  <br />
+            choose   <span style={
+              {
+                color: '#775EFB',
+                fontSize: 48
+              }
+            }>Copywing</span>
+          </h3>
+          <div>
+            <div>
+
+              <div className=''>
+                <span style={
+                  {
+                    color: '#775EFB',
+                    fontSize: 20
+                  }
+                }><b>Expert traders</b></span>
+                <p className="p mb-2" style={{
+                  color: "#C3C3C3"
+                }}>
+                  Gain access to a diverse pool of expert traders with proven track records of success. Follow, learn from, and replicate the strategies of top-performing traders to optimize your investment outcomes.
+                </p>
+              </div>
             </div>
-            <div className="md:max-w-[300px] pb-4 mt-4 md:mt-10 md:mx-0">
-              <img src="/images/hero-ilustration.png" />
+            <div className=''>
+              <span style={
+                {
+                  color: '#775EFB',
+                  fontSize: 20
+                }
+              }><b>User friendly interface</b></span>
+              <p className="p mb-2" style={{
+                color: "#C3C3C3"
+              }}>
+                Our platform is designed with simplicity and usability
+                in mind. Whether you're a seasoned trader or just
+                getting started, you'll find our intuitive interface
+                easy to navigate and use.   </p>
+            </div>
+            <div className=''>
+              <span style={
+                {
+                  color: '#775EFB',
+                  fontSize: 20
+                }
+              }><b>Security & Reliability</b></span>
+              <p className="p mb-2" style={{
+                color: "#C3C3C3"
+              }}>
+                We take the security of your assets and data seriously. Our platform employs robust security measures and adheres to industry best practices to ensure the safety and integrity of your investments.
+              </p>
             </div>
           </div>
+        </div>
+        <div className="md:mx-4" style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center"
+        }}>
+          <img src="/newsectionimages/card-1.png" className='my-3' />
+          <img src="/newsectionimages/card-2.png" className='my-3' />
+        </div>
+      </div>
+      <div data-aos="fade-up" data-w-id="b3fd95f7-10b2-f103-e2b0-e28f97e2adb1"
+        className="  my-5 py-5"
+      >
+        <div className="md:p-8 pb-6  md:max-w-[90%]" style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          margin: "auto",
+        }}>
+          <h3 className="h2 text-center" style={{
+            fontSize: 48,
+            lineHeight: "60px"
+          }}> Get started with <span style={
+            {
+              color: '#775EFB',
+              fontSize: 48
+            }
+          }>Copywing</span>
+            <br />     and get benifits
+          </h3>
+          <p className="p mt-2 mb-2 mx-auto text-center" style={{
+            color: "#C3C3C3",
+            fontSize: 16
+          }}>
+            Has a variety of features that make it the best place to start copy trading   </p>
+
+          <div className='d-md-flex justify-content-between pt-12' >
+            <div className='card-10 p-5'>
+              <img src="/newsectionimages/box 1.png" className='d-block mb-4 mx-auto' style={{
+              }} />
+              <h4 className='card-h4'>Divident Distribution</h4>
+              <p className='card-p'>Dividends are distributed with every
+                transaction, based on the
+                platform’s profits.</p>
+            </div>
+            <div className='card-10 p-5'>
+              <img src="/newsectionimages/cash-withdrawal 1.png" className='d-block mb-4 mx-auto' style={{
+              }} />
+              <h4 className='card-h4'>Sell TDF Tokens anytime</h4>
+              <p className='card-p'>You can sell your TDF tokens at any
+                time through our Pool
+                Based smart contract.</p>
+            </div>
+            <div className='card-10 p-8'>
+              <img src="/newsectionimages/payment-method 1.png" className='d-block mb-4 mx-auto' style={{
+              }} />
+              <h4 className='card-h4'>TDF Burn on withdrawal</h4>
+              <p className='card-p'>Dividends are distributed with every
+                transaction, based on the
+                platform’s profits.</p>
+            </div>
           </div>
+        </div>
+      </div>
+      <div className='GovernedCommunity text-center py-28 container'>
+        <h4 className='text-3xl font-bold text-white'>
+          <span className='text-white'>Copywing </span> is Governed by the <br /> <span className='text-[#9B59B6]'>community</span>
+
+        </h4>
+        <p className='mt-5 md:w-1/2 mx-auto mb-5 text-white'>To encourage long-term holding, dividends are distributed periodically, creating a strong incentive for users to retain their CWN tokens. This aligns the interests of our community with the growth of the platform.</p>
+
+        <a href='https://google.com' className='bg-[#9B59B6] mt-5 rounded-3xl py-2 mx-md-5' style={{ fontSize: 14 }}>Click here to start buying & being a part of CWN</a>
+      </div>
+      <div data-aos="fade-up" data-w-id="b3fd95f7-10b2-f103-e2b0-e28f97e2adb1"
+        className="df-item mt-10  df-item_last  mb-5 md:flex  justify-between" style={{ border: 'none ' }}
+      >
+        <div className="p-8 md:ml-5 md:max-w-[55%]">
+
+          <h3 className="text-2xl font-semibold mt-5 text-white">Why you should invest in <span className="text-[#9B59B6]">Copywing?</span></h3>
+          <ul className="mt-5">
+            <li className="text-sm mb-3 flex">
+              <img className="w-6 mr-4" src="/tickgreen.svg" />
+              <p className="text-white">Copywing is completely decentralized & works on smart contract</p>
+            </li>
+            <li className="text-sm mb-3 flex">
+              <img className="w-6 mr-4" src="/tickgreen.svg" />
+              <p className="text-white">  Your investment in CWN directly benefits from the success of CopyWing.</p>
+            </li>
+            <li className="text-sm mb-3 flex">
+              <img className="w-6 mr-4" src="/tickgreen.svg" />
+              <p className="text-white"> Participate in a Growing Community: Engage with fellow traders and investors who share your goals and aspirations.</p>
+            </li>
+            <li className="text-sm mb-3 flex">
+              <img className="w-6 mr-4" src="/tickgreen.svg" />
+              <p className="text-white">As we expand our offerings and improve our platform, CWN holders will have a voice in the future direction of CopyWing.</p>
+            </li>
+          </ul>
+        </div>
+        <div className="md:max-w-[300px] pb-4 mt-4 md:mt-10 md:mx-0">
+          <img src="/images/hero-ilustration.png" />
+        </div>
+      </div>
+    </div>
   )
 }
 
